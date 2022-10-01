3b8ad8e2783012f93201f56a461340b10dd0a428
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 16:17:32 -0500

    Update sumacheques.rkt

--- LAB/sumacheques.rkt
+++ LAB/sumacheques.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname sumacheques) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define-struct cheque (num banco monto))
 
 ;(define uncheque (make-cheque 258 'Colpatria 200000))
