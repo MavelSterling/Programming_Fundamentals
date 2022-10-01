98e876149dde0270309aad78cde5c9143d1b7cad
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 16:23:47 -0500

    Update sexo.rkt

--- LAB/sexo.rkt
+++ LAB/sexo.rkt
@@ -1,7 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname sexo) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;ZONA DE DEFINICIONES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -----------------------------     ZONA DE DEFINICIONES  -------------------------------------;;
 
 (define arbol-g (list 'mark
                       (list 'pedro
