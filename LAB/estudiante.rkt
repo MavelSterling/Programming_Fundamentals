6596a7b50e117bab6ec8247e29839daeb0c5fcd4
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 16:26:34 -0500

    Update estudiante.rkt

--- LAB/estudiante.rkt
+++ LAB/estudiante.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname estudiante) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;;ANALISIS Y DEFINICION DE DATOS:
 (define-struct estudiante (cod plan nom ap doc))
 ;;Un estudiante representa a un estudiante de Univalle, con sus datos más importantes.
