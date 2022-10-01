5be8e1c76a00fd530b248a841c574d6977abdb36
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 16:02:54 -0500

    Update punto 4.rkt

--- LAB/punto 4.rkt
+++ LAB/punto 4.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |punto 4|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; NOMBRES: Mavelyn Sterling, Sebastian Potes Blandon
 
 ; PLAN: 3743, 2711
